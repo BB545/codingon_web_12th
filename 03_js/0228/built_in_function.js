3d23386f1b5c3c820221c6f0f3d31f69fbc91769
BB545 <black1016@hanmail.net>
2024-02-29 12:05:04 +0900

    study: JS 내장함수, escape notation 연습

--- 03_js/0228/built_in_function.js
+++ 03_js/0228/built_in_function.js
@@ -8,15 +8,33 @@ console.log(Math.max(1,3,2)) //주어진 값 중 가장 큰값
 console.log(Math.min(1,3,2)) //주어진 값 중 가장 작은 값
 
 console.log('--------문자열 함수')
-let msg="This is my first message";
-let result1=msg.includes('my') // true //포함하니
-let result2=msg.includes('your') //false //포함하니
-let result3=msg.startsWith('This')// true //시작하니
-let result4=msg.startsWith('this')//false //시작하니
-let result5=msg.endsWith('e') // true //e로 끝나니
-let result6=msg.indexOf('my') // 8 //순서 몇번째
-let result7=msg.replace('first', 'second') //first를 second로 바꿔줘
-let result8=msg.toUpperCase() //다 대문자로 바꿔줘
+// let msg="This is my first message";
+// let result1=msg.includes('my') // true //포함하니
+// let result2=msg.includes('your') //false //포함하니
+// let result3=msg.startsWith('This')// true //시작하니
+// let result4=msg.startsWith('this')//false //시작하니
+// let result5=msg.endsWith('e') // true //e로 끝나니
+// let result6=msg.indexOf('my') // 8 //순서 몇번째
+// let result7=msg.replace('first', 'second') //first를 second로 바꿔줘
+// let result8=msg.toUpperCase() //다 대문자로 바꿔줘
+
+// console.log(result1, result2, result3, result4)
+// console.log(result5, result6, result7, result8)
+
+let str="  This is my first message   ";
+let result1=str.toLowerCase();
+let result2=str.trim(); //시작 끝 공백 없앰
+let result3=str.trimStart(); //시작 공백 없앰
+let result4=str.trimEnd(); //끝 공백 없앰
+let result5=str.split(' '); //'공백', 공백으로 분리(일반적으로 단어 단위)
+let result6=str.split(''); //''공백없음, 글자 하나하나 분리
 
 console.log(result1, result2, result3, result4)
-console.log(result5, result6, result7, result8)
+console.log(result5, result6)
+
+//escape notation \' : 문자열 인식을 위해 사용하는 것이 아니라 '문자를 표현하고 싶을 때, \n : 줄바꿈
+const msg1=" this is \' my first message";
+const msg2=" this is \n my first message";
+
+console.log(msg1);
+console.log(msg2);
