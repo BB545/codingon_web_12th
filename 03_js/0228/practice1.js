21762574e860840ab46391142baf6fbbf2a4aee2
BB545 <black1016@hanmail.net>
2024-02-29 13:40:42 +0900

    study: JS prompt, Number, toString 함수 연습

---
+++ 03_js/0228/practice1.js
@@ -0,0 +1,13 @@
+//키보드에서 사용자로부터 입력할때
+// let name=prompt('이름을 입력하세요')
+// console.log(name);
+
+// let age=prompt('나이')
+// console.log(age-1);
+
+//prompt 명령을 비롯해서 키보드로부터 데이터를 입력받는 형태는 모두 문자형으로 들어온다.
+
+let x='10';
+console.log(Number(x)+10) //1010=20 문자를 숫자로
+let y=10;
+console.log(y.toString(x)+10) //10->1010 숫자를 문자로
