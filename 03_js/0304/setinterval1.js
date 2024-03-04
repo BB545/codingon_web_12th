d106e75bcc3bd327921044add30d445f1da9ee26
BB545 <black1016@hanmail.net>
2024-03-04 10:27:03 +0900

    study: JS setTimeout, setInterval 연습

---
+++ 03_js/0304/setinterval1.js
@@ -0,0 +1,11 @@
+let addNum=0;
+let subNum=1000;
+let increase=setInterval(function(){
+    addNum++;
+    console.log("addNum ++", addNum);
+}, 1000)
+
+let decrease=setInterval(function(){
+    subNum--;
+    console.log("subNum --", subNum);
+}, 1000)
