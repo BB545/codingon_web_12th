0a65606b2466cfcf68f86bb77b91403b8cb8425a
노현희 <148606294+BB545@users.noreply.github.com>
2024-03-26 11:30:42 +0900

    study: node.js - sync, non_blocking, 구조분해, math, import 연습

---
+++ 03_js/0326/05/app.js
@@ -0,0 +1,9 @@
+import {a,b} from './var.js'
+import returnString from './func.js'
+
+//import from es6 문법
+
+console.log(returnString)
+console.log(a,b)
+
+//import할때는 json파일 "type": "module"작성해줘야함
