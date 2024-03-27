da66802094ad961f62f07c6e4108ac40897797a0
노현희 <148606294+BB545@users.noreply.github.com>
2024-03-27 11:29:54 +0900

    study: node.js - ejs 모듈 불러오기 연습

--- 03_js/0327/13_1/app.js
+++ 03_js/0327/13_1/app.js
@@ -4,6 +4,7 @@ const PORT = 8000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views'); //두개 이름 같아야함
+app.use(express.static('public')) //변경되지 않아야하는 폴더 등록
 
 
 app.get('/', (req,res)=>{
@@ -16,11 +17,11 @@ app.get('/about', (req,res)=>{
 
 app.get('/create', (req,res)=>{
     res.render('create', {title: '작성하기'})
-})
+}) //지정된 경로가 있을때 get사용
 
 app.use((req,res)=>{
     res.status(404).render('404')
-})
+}) //use를 사용하는 이유는 지정된 경로가 없기 때문
 
 app.listen(PORT, ()=>{
     console.log(`Server is running ${PORT}`)
