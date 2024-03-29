3abd96e627958520677de984cdc4a4f30fb3a06c
BB545 <black1016@hanmail.net>
2024-03-29 14:00:55 +0900

    study: node.js - form POST GET 실습

---
+++ 03_js/0329/04_form_get_post_prac/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    console.log('루트에서 들어옴')
+    res.render('index')
+})
+
+app.get('/login', (req,res)=>{
+    console.log('로그인 GET 요청')
+    console.log(req.query)
+    res.render('result', {title:'GET 로그인 화면', userInfo:req.query})
+})
+
+app.post('/login', (req,res)=>{
+    console.log('로그인 POST 요청')
+    console.log('req body--->', req.body)
+    res.render('result', {title:'POST 로그인 화면', userInfo:req.body})
+})
+
+app.listen(8080, ()=>{
+    console.log('Server is running...')
+})
