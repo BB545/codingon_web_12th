e5284108b9cfe138268b042f67656edfb36dba32
BB545 <black1016@hanmail.net>
2024-04-09 10:36:11 +0900

    study: node.js - sequelize 설치 및 명령어 연습

---
+++ 03_js/0409/01_sequelize/seeders/20240409005747-userData.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Users', [{
+      userId: '1',
+      name: 'Kim',
+      engName: 'A',
+      createdAt: '2024-04-09',
+      updatedAt: '2024-04-09'
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
