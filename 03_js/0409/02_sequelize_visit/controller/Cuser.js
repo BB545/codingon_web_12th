588e45da737e31e86832c3fc3e3a4dcd3cd0e51f
BB545 <black1016@hanmail.net>
2024-04-12 13:50:14 +0900

    study: node.js - sequelize controller 추가 및 rest client 기초

--- 03_js/0409/02_sequelize_visit/controller/Cuser.js
+++ 03_js/0409/02_sequelize_visit/controller/Cuser.js
@@ -11,25 +11,66 @@ exports.get_signin = (req,res)=>{
 
 //회원가입 요청
 exports.post_signup = (req,res)=>{
-
+    console.log('controller > Cuser.js > post_signup')
+    User.create({
+        userid: req.body.userid,
+        name: req.body.name,
+        pw: req.body.pw
+    }).then( result =>{
+        console.log('Created >', result)
+        res.send(result)
+    })
 }
 
 //로그인 요청
 exports.post_signin = (req,res)=>{
-    
+    onsole.log('post방식으로 signin',req.body);
+    User.findOne({
+        raw: true,
+        where: {
+            userid: req.body.userid,
+            pw: req.body.pw
+        }
+    }).then((result) => {
+        console.log('findOne', result);
+        if (result) {
+            res.send({ isLogin: true, userInfo: result });
+        } else {
+            res.send({ isLogin: false });
+        }
+    })
 }
 
 //회원 정보 수정 페이지
 exports.post_profile = (req,res)=>{
-    
+    console.log('post방식으로 profile',req.body);
+    User.findOne({
+        where :{ userid: req.body.userid}
+    }).then(result =>{
+        console.log('findOne :post_profile > ', result)
+        if(result)   res.render('profile', {data: result})
+    })
 }
 
 //회원 정보 수정
 exports.edit_profile = (req,res)=>{
-    
+    console.log('controller > edit_profile' , req.body);
+    User.update({
+        name:req.body.name,
+        pw: req.body.pw
+    },{
+        where : {id:req.body.id}
+    }).then( result =>{
+        res.send('회원정보수정')}
+    )
 }
 
 //회원 정보 삭제
 exports.delete_profile = (req,res)=>{
-    
+    console.log('controller > delete_profile' , req.body);
+    User.destrory({
+        where : {id:req.body.id}
+    }).then( result =>{
+        res.send({deleteId: req.body.id})}
+    )
 }
