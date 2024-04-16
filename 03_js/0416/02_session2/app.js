521e874c1634e31fe80e719816f3e7af68cf11fb
BB545 <black1016@hanmail.net>
2024-04-16 13:40:41 +0900

    study: node.js - session 로그인 만들기 실습 풀이 및 로그인 유지

--- 03_js/0416/02_session2/app.js
+++ 03_js/0416/02_session2/app.js
@@ -22,8 +22,13 @@ app.use(session({
 const userInfo = { id: 'banana', pw: '1234'};
 
 app.get('/', (req,res)=>{
-    // console.log('req.sesion.user >', user);
-    res.render('index')
+    const user = req.session.user;
+    console.log('req.sesion.user >', user);
+    if(user !== undefined){
+        res.render('index', {isLogin: true, user: user})
+    } else{
+        res.render('index', {isLogin: false})
+    }
 })
 
 app.get('/login', (req,res)=>{
@@ -32,13 +37,23 @@ app.get('/login', (req,res)=>{
 
 app.post('/login', (req,res)=>{
     const { id, pw } = req.body;
-    if (id === userInfo.id && pw === userInfo.pw) {
-        req.session.user = { id: userInfo.id, pw: userInfo.pw };
-        console.log(req.sessionID);
-        console.log(req.session.user);
-        res.render('include', { user: req.session.user.id });
-    } else {
-        res.send('로그인 실패');
+    if(id===userInfo.id && pw===userInfo.pw){
+        req.session.user=id;
+        res.redirect('/')
+    } else{
+        res.send('로그인 실패')
+    }
+})
+
+app.get('/logout', (req,res)=>{
+    const user = req.session.user;
+    if(user!==undefined){
+        req.session.destroy(err=>{
+            res.redirect('/')
+            // 혹시나 에러가 생기면 리다이렉트해서 /로 가라
+        })
+    } else{
+        res.send('잘못된 접근입니다.')
     }
 })
 
