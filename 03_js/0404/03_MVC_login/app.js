23e950f380f2da9000fae7a5fab3fc8a9328b200
BB545 <black1016@hanmail.net>
2024-04-23 12:15:17 +0900

    study: node.js - MVC, axios, login 실습

--- 03_js/0404/03_MVC_login/app.js
+++ 03_js/0404/03_MVC_login/app.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const app = express();
 
+// body-parser를 사용하여 요청 본문 파싱 설정
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.set(express.urlencoded({extended:true}));
-app.set(express.json());
+// app.set(express.urlencoded({extended:true}));
+// app.set(express.json());
 
 const userRouter = require('./routes/index');
 const { userInfo } = require('./model/Cuser');
 app.use('/', userRouter)
 
 app.post('/axios', (req,res)=>{
+    const userInfoO = userInfo();
+    const {userid,userpw} = userInfoO;
     const {id,pw} = req.body;
-    if (id === userInfo.userid && pw === userInfo.userpw) {
+    console.log(userid);
+    console.log(id);
+    if (id === userid && pw === userpw) {
         res.send(req.body)
     } else {
-        res.status(401).send()
+        res.send({response: false})
     }
 })
 
