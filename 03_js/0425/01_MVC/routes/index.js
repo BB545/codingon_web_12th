2801f325196b4583e363433068eb47bbcbbdafd7
BB545 <black1016@hanmail.net>
2024-04-25 12:12:25 +0900

    study: MVC - MySQL 연결 복습 router로 경로 설정

---
+++ 03_js/0425/01_MVC/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/Cvisitor');
+
+router.get('/', controller.main);
+router.get('/visitors', controller.allVisitorList);
+router.post('/addvisitor', controller.addVisitor);
+router.get('/visitormodify/:id', controller.getShowAVisitor);
+router.patch('/visitoreditDo', controller.patchVisitor);
+router.delete('/visitordelete', controller.deleteVisitor);
+
+module.exports = router;
