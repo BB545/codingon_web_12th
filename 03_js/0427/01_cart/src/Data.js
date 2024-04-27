065266de0c07de7a7135b4af8475e8a05676847c
BB545 <black1016@hanmail.net>
2024-04-27 12:56:56 +0900

    study: cart 만들기 기본 페이지 연습

---
+++ 03_js/0427/01_cart/src/Data.js
@@ -0,0 +1,33 @@
+let shopItemsData = [
+    {
+        id: 'cloth1',
+        name: 'Casual shirt',
+        price: 15,
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        img: 'images/image-1.jpg'
+    },
+
+    {
+        id: 'cloth2',
+        name: 'Office shirt',
+        price: 50,
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        img: 'images/image-2.jpg'
+    },
+
+    {
+        id: 'cloth3',
+        name: 'Casual dress',
+        price: 23,
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        img: 'images/image-3.jpg'
+    },
+
+    {
+        id: 'cloth4',
+        name: 'Casual shirt',
+        price: 45,
+        desc: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        img: 'images/image-4.jpg'
+    },
+]
