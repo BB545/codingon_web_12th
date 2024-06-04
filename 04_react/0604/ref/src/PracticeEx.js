4765af601962c39bf63f248e9a7d71bcf842fbc2
BB545 <black1016@hanmail.net>
2024-06-04 10:46:12 +0900

    study: 리액트 - 클래스형 컴포넌트 콜백함수, 내장함수 Ref 실습 테이블 수정

--- 04_react/0604/ref/src/PracticeEx.js
+++ 04_react/0604/ref/src/PracticeEx.js
@@ -3,35 +3,36 @@ import React, { Component } from 'react'
 export default class PracticeEx extends Component {
     state = {
         data: [
-        {
-            writer: '민수',
-            title: '안뇽',
-        },
-        {
-            writer: '지민',
-            title: '하이하이',
-        },
-        {
-            writer: '희수',
-            title: '멋쟁이',
-        },
-    ]}
+            {
+                writer: '민수',
+                title: '안뇽',
+            },
+            {
+                writer: '지민',
+                title: '하이하이',
+            },
+            {
+                writer: '희수',
+                title: '멋쟁이',
+            },
+        ]
+    }
 
     writerInput = React.createRef();
     titleInput = React.createRef();
 
     handleFocus = () => {
-        if(this.writerInput.current.value.trim().length === 0){
+        if (this.writerInput.current.value.trim().length === 0) {
             this.writerInput.current.focus();
-        } else if(this.titleInput.current.value.trim().length === 0){
+        } else if (this.titleInput.current.value.trim().length === 0) {
             this.titleInput.current.focus();
         } else {
             const newInform = this.state.data.concat({
                 writer: this.writerInput.current.value,
                 title: this.titleInput.current.value
             })
-    
-            this.setState({data: newInform})
+
+            this.setState({ data: newInform })
         }
     }
 
@@ -43,16 +44,22 @@ export default class PracticeEx extends Component {
                 <button onClick={this.handleFocus}>작성</button>
 
                 <table border={1} width={500} style={{ margin: 'auto' }}>
-                    <th>번호</th>
-                    <th>제목</th>
-                    <th>작성자</th>
-                    {this.state.data.map((value, index) => {
-                        return <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{value.title}</td>
-                            <td>{value.writer}</td>
+                    <thead>
+                        <tr>
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>작성자</th>
                         </tr>
-                    })}
+                    </thead>
+                    <tbody>
+                        {this.state.data.map((value, index) => {
+                            return <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{value.title}</td>
+                                <td>{value.writer}</td>
+                            </tr>
+                        })}
+                    </tbody>
                 </table>
             </>
         )
