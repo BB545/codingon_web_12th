9ed92eb8e68ca44c67cebec4bc92838d670158ee
BB545 <black1016@hanmail.net>
2024-06-07 12:56:43 +0900

    study: 리액트 - scss 원 움직이기 실습

---
+++ 04_react/0607/styles/src/PracticeEx2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/PracticeEx2.scss'
+
+export default function PracticeEx2() {
+  return (
+    <div className='container2'>
+        <div className='circle pink'></div>
+        <div className='circle yellow'></div>
+        <div className='circle green'></div>
+    </div>
+  )
+}
