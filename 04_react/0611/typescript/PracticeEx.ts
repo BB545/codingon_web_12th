186e46253c8688b9dbc3c1a19abf20746f2c3486
BB545 <black1016@hanmail.net>
2024-06-11 11:03:00 +0900

    study: 타입스크립트 - 기초 타입스크립트 실습

---
+++ 04_react/0611/typescript/PracticeEx.ts
@@ -0,0 +1,10 @@
+let olimpic_newgame: readonly [object, boolean] = [
+    {
+        name: '쇼트트랙',
+        type: '혼성계주',
+    },
+    true,
+]
+
+// olimpic_newgame[1] = false; // 수정 안됨
+console.log(olimpic_newgame);
