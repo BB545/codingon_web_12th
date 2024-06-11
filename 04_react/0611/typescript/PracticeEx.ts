8e9acbe33efc7957be42d5bb93000774a9643c41
BB545 <black1016@hanmail.net>
2024-06-11 11:50:13 +0900

    study: 타입스크립트 - 사용자 지정 타입 - 인터페이스 실습

--- 04_react/0611/typescript/PracticeEx.ts
+++ 04_react/0611/typescript/PracticeEx.ts
@@ -8,3 +8,27 @@ let olimpic_newgame: readonly [object, boolean] = [
 
 // olimpic_newgame[1] = false; // 수정 안됨
 console.log(olimpic_newgame);
+
+// interface 실습
+interface Game {
+    title: string,
+    price: number,
+    desc?: string,
+    category: string,
+    platform: string,
+}
+
+let heroGame_A: Game = {
+    title: 'DC 언체인드',
+    price: 50000,
+    desc: 'DC 히어로 & 빌런 각각의 개성은 물론, 액션의 재미까지!',
+    category: '액션',
+    platform: '모바일',
+}
+
+let heroGame_B: Game = {
+    title: 'MARVEL 퓨처파이트',
+    price: 65000,
+    category: '롤플레잉',
+    platform: '모바일',
+}
